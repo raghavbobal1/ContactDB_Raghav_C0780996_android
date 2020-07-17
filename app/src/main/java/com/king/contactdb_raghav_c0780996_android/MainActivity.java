3a445a2c7751c11ab39a5d6d2c6db1b5a9e36de1
raghavbobal1 <raghavbobal001@gmail.com>
2020-07-17 07:02:23 -0400

    initial commit

---
+++ app/src/main/java/com/king/contactdb_raghav_c0780996_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.king.contactdb_raghav_c0780996_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

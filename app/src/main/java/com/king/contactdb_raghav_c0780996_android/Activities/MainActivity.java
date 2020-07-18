0009c93b40333e2d9448f4a088ccf8aed74844e6
raghavbobal1 <raghavbobal001@gmail.com>
2020-07-17 21:23:02 -0400

    completed ui

--- app/src/main/java/com/king/contactdb_raghav_c0780996_android/Activities/MainActivity.java
+++ app/src/main/java/com/king/contactdb_raghav_c0780996_android/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.king.contactdb_raghav_c0780996_android;
+package com.king.contactdb_raghav_c0780996_android.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.king.contactdb_raghav_c0780996_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
